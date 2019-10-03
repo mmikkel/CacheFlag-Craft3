df6e0bff2f4d4647002e4a3a0d05a80f902e13aa
Simon Müller <simon.d.mueller@gmail.com>
2019-10-03 10:43:00 +0200

    Create cache-flag.php

---
+++ src/translations/de/cache-flag.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Cache Flag plugin for Craft CMS 3.x
+ *
+ * Flag and clear template caches.
+ *
+ * @link      https://vaersaagod.no
+ * @copyright Copyright (c) 2018 Mats Mikkel Rummelhoff
+ */
+/**
+ * @author    Mats Mikkel Rummelhoff
+ * @package   CacheFlag
+ * @since     1.0.0
+ */
+return [
+    'Cache Flag plugin loaded' => 'Cache Flag Plugin wurde geladen',
+];
